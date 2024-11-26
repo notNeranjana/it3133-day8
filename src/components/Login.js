60300d5978140137e9da4e046525f577a4f50dc4
Neranjana Prasad <neranjanaone@gmail.com>
2024-11-26 14:35:49 +0530

    feat: add useNavigation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const authenticate = () => {
+    navigate("/dashboard");
+  };
+
   return (
     <div>
       <h1>Login</h1>
 
-      <button onClick={() => {}}>Login</button>
+      <button onClick={authenticate}>Login</button>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
