d2cb6403f7ec28d58b874ec947179590978fdacc
Neranjana Prasad <neranjanaone@gmail.com>
2024-11-26 15:09:19 +0530

    feat: implement authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { users } from "../data/users";
+import {useState} from "react";
 
 const Login = () => {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const authenticate = () => {
+    if (!username || !password) {
+        return setError("Username and password are required!");
+    }
+
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+        return setError("User not found!");
+    }
+
+    if (user.password !== password) {
+        return setError("Password is wrong!");
+    }
+
     navigate("/dashboard");
   };
 
@@ -12,7 +32,32 @@ const Login = () => {
     <div>
       <h1>Login</h1>
 
+      <div className="">
+        <p>Username</p>
+        <input
+          type="text"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div className="">
+        <p>Email</p>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
       <button onClick={authenticate}>Login</button>
+
+      {error && (
+        <div>
+          <p>Error :</p>
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/data/users.js
@@ -0,0 +1,6 @@
+export const users = [
+  {
+    username: "neranjana",
+    password: "Asd@1234",
+  },
+];
